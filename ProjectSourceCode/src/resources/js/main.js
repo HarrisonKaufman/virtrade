6749946f2f889d9cd106b5e48a6f99d7b9c6c4b7
Dylan Spektor <dylanspek07@gmail.com>
2026-03-29 12:01:30 -0600

    add chart widget partial and loadChart js function

---
+++ ProjectSourceCode/src/resources/js/main.js
@@ -0,0 +1,6 @@
+async function loadChart(symbol = "NASDAQ:AAPL") {
+  window.CHART_SYMBOL = symbol;
+  const res = await fetch("../../views/partials/chart-widget.hbs");
+  const html = await res.text();
+  document.getElementById("chart-container").innerHTML = html;
+}
