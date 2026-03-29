b66a2cc61a3613cd41bb9523c7a1b966033d9c3a
Mihai-Alexandru Radu <mira3628@colorado.edu>
2026-03-28 20:57:22 -0600

    preliminary files for index.js

---
+++ ProjectSourceCode/index.js
@@ -0,0 +1,15 @@
+const express = require('express'); // To build an application server or API
+const app = express();
+const handlebars = require('express-handlebars'); //to enable express to work with handlebars
+const Handlebars = require('handlebars'); // to include the templating engine responsible for compiling templates
+const path = require('path');
+const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
+const bodyParser = require('body-parser');
+const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
+const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part C.
+
+const hbs = handlebars.create({
+  extname: 'hbs',
+  layoutsDir: __dirname + '/views/layouts',
+  partialsDir: __dirname + '/views/partials',
+});
